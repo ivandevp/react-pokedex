7a500a36a16edd24c7e6f4bcc651f7d93c435464
Ivan Medina <medinadiazivan@gmail.com>
2018-05-16 20:23:39 -0500

    sets redux structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Pokedex from './Pokedex';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>
+        <Pokedex />
+    </Provider>, 
+    document.getElementById('root')
+);
